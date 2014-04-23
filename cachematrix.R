e5c687ddda7b96724561abc6681e4a7300ed67fe
Logan J Travis <loganjtravis@gmail.com>
2014-04-22 21:56:24 -0700

    Create Test Functions
    Create test functions for matrix inversion and caching.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,43 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invertX <- NULL
+    
+    ##
+    set <- function(y) {
+        x <<- y
+        invertX <<- NULL
+    }
+    
+    ##
+    get <- function() x
+    
+    ##
+    clear <- function() set(matrix())
+    
+    ##
+    setInvert <- function() invertX <<- solve(x)
+    
+    ##
+    getInvert <- function() invertX
+    
+    ##
+    clearInvert <- function() invertX <<- NULL
+    
+    ##
+    list(set = set, get = get, clear = clear, setInvert = setInvert,
+         getInvert = getInvert, clearInvert = clearInvert)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    if(is.null(x$getInvert())){
+        message("creating inverse matrix")
+        x$setInvert()
+    } else {
+        message("retrieving from cache")
+    }
+    x$getInvert()
 }
